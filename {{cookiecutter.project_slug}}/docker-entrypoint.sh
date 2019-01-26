33ebb26ee4b8c685ad219b839db99a006e32b47c
Ant Somers <antsomers@gmail.com>
2019-01-26 05:58:36 +0300

    Fix #8 Add or logic for each command This solution seems nasty. There must better solution. Rework required.

--- {{cookiecutter.project_slug}}/docker-entrypoint.sh
+++ {{cookiecutter.project_slug}}/docker-entrypoint.sh
@@ -12,23 +12,23 @@ die() {
 while (( $# )); do
     case $1 in
         migrate)
-            python manage.py migrate
+            python manage.py migrate || exit $?
             ;;
         collectstatic)
-            python manage.py collectstatic --noinput
+            python manage.py collectstatic --noinput || exit $?
             ;;
         coverage)
-            coverage run --source='.' manage.py test && coverage report
+            coverage run --source='.' manage.py test && coverage report || exit $?
             ;;
         run_dev)
-            python manage.py runserver 0:8000
+            python manage.py runserver 0:8000 || exit $?
             ;;
         run)
-            gunicorn -c gunicorn_config.ini media_observation_database.wsgi
+            gunicorn -c gunicorn_config.ini media_observation_database.wsgi || exit $?
             ;;
         pass)
             if [ "$2" ]; then
-                python manage.py "$2"
+                python manage.py "$2" || exit $?
                 shift
             else
                 die 'ERROR: "pass" requires a non-empty option argument.'
